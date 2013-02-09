468894856a18d4a116b940e657fde8091ca2fcc4
vdehorta <dehorta.v@sfeir.com>
2013-02-09 23:55:45 +0100

    ajout tableau restos en dur

---
+++ src/main/webapp/js/controller/restaurantsCtrl.js
@@ -0,0 +1,21 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vivien
+ * Date: 09/02/13
+ * Time: 23:26
+ * To change this template use File | Settings | File Templates.
+ */
+
+function restaurantsCtrl($scope) {
+
+    $scope.nextId = 3;
+
+    $scope.restaurants = [
+        {id:1, name:'Subway', address:'1 place des Palmiers 92200 Neuilly-sur-Seine', description:'Sandwicherie rapide. Sandwichs de taille et composition personnalisables.'},
+        {id:2, name:'Quick', address:'Centre Commercial les 4 Temps 92300 Courbevoie', description:'Fast-food, burgers, frites'}];
+
+    $scope.addRestaurant = function() {
+        $scope.restaurants.push({id:$scope.nextId, name:$scope.nextName, address:$scope.nextAddress, description:$scope.nextDescription});
+        $scope.nextId ++;
+    };
+}

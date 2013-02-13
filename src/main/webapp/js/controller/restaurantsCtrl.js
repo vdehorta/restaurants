998fcc890cb3af9cba72eb3dbd9e1c3bfbf3bdd0
vdehorta <vdehorta@gmail.com>
2013-02-13 17:31:35 +0100

    "en résolution du bug search".

--- src/main/webapp/js/controller/restaurantsCtrl.js
+++ src/main/webapp/js/controller/restaurantsCtrl.js
@@ -1,21 +1,103 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Vivien
- * Date: 09/02/13
- * Time: 23:26
- * To change this template use File | Settings | File Templates.
- */
+var map;
 
-function restaurantsCtrl($scope) {
+var service;
 
-    $scope.nextId = 3;
+var infowindow = new google.maps.InfoWindow();
 
-    $scope.restaurants = [
-        {id:1, name:'Subway', address:'1 place des Palmiers 92200 Neuilly-sur-Seine', description:'Sandwicherie rapide. Sandwichs de taille et composition personnalisables.'},
-        {id:2, name:'Quick', address:'Centre Commercial les 4 Temps 92300 Courbevoie', description:'Fast-food, burgers, frites'}];
-
-    $scope.addRestaurant = function() {
-        $scope.restaurants.push({id:$scope.nextId, name:$scope.newName, address:$scope.newAddress, description:$scope.newDescription});
-        $scope.nextId ++;
+function initialize() {
+    var mapOptions = {
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     };
+    map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+    // Try HTML5 geolocation
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(handleGeolocationSuccess, function() {
+            handleNoGeolocation(true);
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+    }
 }
+
+function handleGeolocationSuccess(position) {
+    var pos = new google.maps.LatLng(position.coords.latitude,
+    position.coords.longitude);
+
+    var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'Location found using HTML5.'
+    });
+
+    map.setCenter(pos);
+
+    //launchNearbySearch(pos);
+}
+
+function handleNoGeolocation(errorFlag) {
+    if (errorFlag) {
+        var content = 'Error: The Geolocation service failed.';
+    } else {
+        var content = 'Error: Your browser doesn\'t support geolocation.';
+    }
+
+    var options = {
+        map: map,
+        position: new google.maps.LatLng(60, 105),
+        content: content
+    };
+
+    var infowindow = new google.maps.InfoWindow(options);
+    map.setCenter(options.position);
+}
+
+function launchNearbySearch(posLatLng) {
+
+    service = new google.maps.places.PlacesService(document.getElementById('map_canvas'));
+    var request = {
+        location: posLatLng,
+        radius: 1000,
+        types: ['restaurant']
+    };
+
+    alert("Avant nearbySearch");
+    service.nearbySearch(request, function(results, status) {
+
+        alert("nearbySearch effectuée avec succès ou pas");
+
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            alert("Nearby search success number = " + results.length);
+            displayNearbyRestaurants(results);
+        } else {
+            alert("Erreur pdt la Search request !");
+        }
+    });
+}
+
+function displayNearbyRestaurants(results) {
+
+	alert("displayNearbyRestaurants()");
+
+	for (var i = 0; i < results.length; i++) {
+		createMarker(results[i]);
+	}
+}
+
+function createMarker(place) {
+
+	alert("createMarker() pour la place de nom " + place.name + " et de lieu " + place.vicinity);
+
+	var placeLoc = place.geometry.location;
+	var marker = new google.maps.Marker({
+		map: map,
+		position: place.geometry.location});
+
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.setContent(place.name);
+		infowindow.open(map, this);});
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);

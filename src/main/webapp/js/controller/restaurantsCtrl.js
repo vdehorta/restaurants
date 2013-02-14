cebb2db6e0d7a3b3ced065cd79534b64aba56df0
vdehorta <vdehorta@gmail.com>
2013-02-14 11:25:49 +0100

    version stable avec geolocalisation sans angularjs

--- src/main/webapp/js/controller/restaurantsCtrl.js
+++ src/main/webapp/js/controller/restaurantsCtrl.js
@@ -1,15 +1,9 @@
+const DEFAULT_LATLNG = new google.maps.LatLng(60, 105);
+const DEFAULT_ZOOM = 15;
 var map;
-
-var service;
-
-var infowindow = new google.maps.InfoWindow();
+var infowindow;
 
 function initialize() {
-    var mapOptions = {
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 
     // Try HTML5 geolocation
     if (navigator.geolocation) {
@@ -23,18 +17,54 @@ function initialize() {
 }
 
 function handleGeolocationSuccess(position) {
-    var pos = new google.maps.LatLng(position.coords.latitude,
-    position.coords.longitude);
+    var maPosition = new google.maps.LatLng(position.coords.latitude,
+        position.coords.longitude);
 
-    var infowindow = new google.maps.InfoWindow({
+    //alert ("Succès de la géolocalisation, vous êtes aux coordonnées : " + position.coords.latitude + ", " + position.coords.longitude);
+    afficherMap(maPosition, DEFAULT_ZOOM);
+
+    /* Placement d'un marker à la localisation obtenue par géolocalisation */
+    var markerGeolocation = new google.maps.Marker({
         map: map,
-        position: pos,
-        content: 'Location found using HTML5.'
+        position: maPosition
+    });
+    google.maps.event.addListener(markerGeolocation, 'click', function() {
+        infowindow.setContent("Vous êtes ici.");
+        infowindow.open(map, this);
+    });
+    /* Fin marker geolocation */
+
+    var request = {
+        location: maPosition,
+        radius: 1000,
+        types: ['restaurant']
+    };
+
+    infowindow = new google.maps.InfoWindow();
+
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, handleNearbySearchResult);
+}
+
+function handleNearbySearchResult(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
     });
 
-    map.setCenter(pos);
-
-    //launchNearbySearch(pos);
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
 }
 
 function handleNoGeolocation(errorFlag) {
@@ -43,61 +73,17 @@ function handleNoGeolocation(errorFlag) {
     } else {
         var content = 'Error: Your browser doesn\'t support geolocation.';
     }
+    alert(content);
 
-    var options = {
-        map: map,
-        position: new google.maps.LatLng(60, 105),
-        content: content
-    };
-
-    var infowindow = new google.maps.InfoWindow(options);
-    map.setCenter(options.position);
+    afficherMap(DEFAULT_LATLNG, DEFAULT_ZOOM); //Si impossible de géolocaliser, localisation par défaut
 }
 
-function launchNearbySearch(posLatLng) {
-
-    service = new google.maps.places.PlacesService(document.getElementById('map_canvas'));
-    var request = {
-        location: posLatLng,
-        radius: 1000,
-        types: ['restaurant']
-    };
-
-    alert("Avant nearbySearch");
-    service.nearbySearch(request, function(results, status) {
-
-        alert("nearbySearch effectuée avec succès ou pas");
-
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            alert("Nearby search success number = " + results.length);
-            displayNearbyRestaurants(results);
-        } else {
-            alert("Erreur pdt la Search request !");
-        }
+function afficherMap(poosition, zoom) {
+    map = new google.maps.Map(document.getElementById('map'), {
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: poosition,
+        zoom: zoom
     });
-}
-
-function displayNearbyRestaurants(results) {
-
-	alert("displayNearbyRestaurants()");
-
-	for (var i = 0; i < results.length; i++) {
-		createMarker(results[i]);
-	}
-}
-
-function createMarker(place) {
-
-	alert("createMarker() pour la place de nom " + place.name + " et de lieu " + place.vicinity);
-
-	var placeLoc = place.geometry.location;
-	var marker = new google.maps.Marker({
-		map: map,
-		position: place.geometry.location});
-
-	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent(place.name);
-		infowindow.open(map, this);});
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
